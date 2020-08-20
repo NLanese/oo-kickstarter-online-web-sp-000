18b8d1afa2132dfada5be8d0a3adb57bb0cda9bf
Nick Lanese <nlanese21@gmail.com>
2020-08-20 01:00:09 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,4 +5,11 @@ def initialize(name)
   @backers = []
 end
 
+def add_backer(inputBacker)
+  @backers << inputBacker
+  if !(inputBacker.backed_projects.include?(self))
+    inputBacker.add_project(self)
+  end
+end
+
 end
